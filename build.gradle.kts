73c0c497451e5c8529df5d69a1186407e19c9fdb
zhaoyudong <zhaoyudong@staff.hexun.com>
2026-01-16 16:40:35 +0800

    插件app相关配置项

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,15 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+// 这个plugin需要放在android配置之后,因为需要读取android中的配置项
+apply(plugin = "replugin-plugin-gradle")
+
+configure<com.qihoo360.replugin.gradle.plugin.ReClassConfig> {
+    // 插件名
+    pluginName = "repluginapp"
+    // 宿主app的包名
+    hostApplicationId = "com.demo.androidseedproject"
+    // 宿主app的启动Activity
+    hostAppLauncherActivity = "com.demo.main.ui.SplashActivity"
+}
